dd11245eebed39c34761aa8adb7c9397487f5d37
Erich Purpur <epurpur@gmail.com>
2021-05-11 08:48:28 -0400

    writing inquirer questions

--- employee_tracker.js
+++ employee_tracker.js
@@ -26,15 +26,15 @@ const initialQuestions = () => {
                 break;
             
             case 'View Roles':
-                console.log('Bango!');
+                roles();
                 break;
 
             case 'View Employees':
-                console.log('BaZing!');
+                employees();
                 break;
 
             case 'Exit':
-                console.log('Done for now');
+                console.log('Exiting...');
                 break;
         }
     });
@@ -51,16 +51,53 @@ const departments = () => {
             choices: [
                 'View all departments',
                 'Add a department',
-                'View total budget of a department'
+                'View total budget of a department***'
             ]
         })
         .then((answer) => {
             console.log(answer.deptResponse);
+            //DEPENDING ON RESULT, DO DB QUERIES HERE
         })
-
 }
 
+const roles = () => {
+//asks questions related to roles
+    inquirer
+        .prompt({
+            name: 'rolesResponse',
+            type: 'list',
+            message: 'What would you like to do with the roles?',
+            choices: [
+                'View all roles',
+                'Add a role',
+                'Update employee roles'
+            ]
+        })
+        .then((answer) => {
+            console.log(answer.rolesResponse)
+            //DEPENDING ON RESULT, DO DB QUERIES HERE
+        })
+}
 
+const employees = () => {
+    inquirer
+        .prompt({
+            name: 'employeesResponse',
+            type: 'list',
+            message: 'What would you like to do with the employees?',
+            choices: [
+                'View all employees',
+                'Add an employee',
+                'Update employee role',
+                'Delete employee',
+                'View employees by manager***'
+            ]
+        })
+        .then((answer) => {
+            console.log(answer.employeesResponse);
+            //DEPENDING ON RESULT, DO DB QUERIES HERE
+        })
+}
 
 
 initialQuestions();
